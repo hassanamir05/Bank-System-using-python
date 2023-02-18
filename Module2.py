8b6a9023ad47c3a562da7fbe24720edb684e4f19
M.Hassan Amir <120783382+hassanamir05@users.noreply.github.com>
2023-02-18 15:47:58 +0500

    Updated Mosule2.py

--- Module2.py
+++ Module2.py
@@ -1,77 +1,75 @@
-saving_account_registered = 0
-saving_account_data=[]
+total_employe = 0
+employe_data=[]
 
-def create_acount_saving():
-    global saving_account_registered
+def create_employe():
+    global total_employe
     name=input("Enter your name : ")
-    saving_account_data.append(name)
+    employe_data.append(name)
     cnic=input("Enter your CNIC number : ")
-    saving_account_data.append(cnic)
-    balance=input("Enter Balance : ")
-    saving_account_data.append(balance)
+    employe_data.append(cnic)
+    contact_=input("Enter Contact : ")
+    employe_data.append(contact_)
     address = input("Enter the Address : ")
-    saving_account_data.append(address)
-    saving_account_registered =saving_account_registered+ 1
-    print("Your account number is ", saving_account_registered)
-    print("Account Created Succesfully! ")
+    employe_data.append(address)
+    total_employe = total_employe + 1
+    print("Your ID is ", total_employe)
     print()
     print()
-    print(saving_account_registered)
-def view_saving():
-    global saving_account_registered
-    if saving_account_registered != 0:
-        print("  ", format("Account Holder Name   ", "<25s"), format("Account Holder CNIC   ", "<25s"),
-              format("Account Balance   ", "<25s"), format("Account Holder Address   ", "<25s"))
+def view_employe():
+    global total_employe
+    if total_employe != 0:
+        print("  ", format("Employe Name   ", "<25s"), format("Employe CNIC   ", "<25s"),
+              format("Employe Contact Number  ", "<25s"), format("Employe Address   ", "<25s"))
         print(" ______________________________________________________________________________________________________")
-        for i in range(0, len(saving_account_data), 4):
-            print("  ", format(saving_account_data[i], "<25s"), format(saving_account_data[i + 1], "<25s")
-                  , format(saving_account_data[i + 2], "<25s"), format(saving_account_data[i + 3], "<25s"))
-    elif saving_account_registered == 0:
-        print("No Account Created!")
+        for i in range(0, len(employe_data), 4):
+            print("  ", format(employe_data[i], "<25s"), format(employe_data[i + 1], "<25s")
+                  , format(employe_data[i + 2], "<25s"), format(employe_data[i + 3], "<25s"))
+    elif total_employe == 0:
+        print("No Employe!")
         print()
     print()
 
-def search_saving():
-    global saving_account_registered
-    temp_accnum = eval(input("Enter your account number : "))
-    if ((temp_accnum * 4) <= len(saving_account_data)):
-        i = (temp_accnum - 1) * 4
-        print("  ", format("Account Holder Name   ", "<25s"), format("Account Holder CNIC   ", "<25s"),
-              format("Account Balance   ", "<25s"), format("Account Holder Address   ", "<25s"))
+def search_employe():
+    global total_employe
+    temp_id = eval(input("Enter your ID : "))
+    if ((temp_id * 4) <= len(employe_data)):
+        i = (temp_id - 1) * 4
+        print("  ", format("Employe Name   ", "<25s"), format("Employe CNIC   ", "<25s"),
+              format("Employe Balance   ", "<25s"), format("Employe Address   ", "<25s"))
         print(" ______________________________________________________________________________________________________")
-        if saving_account_data != 0:
-            print("  ", format(saving_account_data[i], "<25s"), format(saving_account_data[i + 1], "<25s")
-                  , format(saving_account_data[i + 2], "<25s"), format(saving_account_data[i + 3], "<25s"))
+        if employe_data != 0:
+            print("  ", format(employe_data[i], "<25s"), format(employe_data[i + 1], "<25s")
+                  , format(employe_data[i + 2], "<25s"), format(employe_data[i + 3], "<25s"))
     else:
         print("No Record Found!")
 
     print("\n\n")
 
-def update_saving():
-    global saving_account_registered
-    print("What do you want to update :\n1.Account Holder Name\n2.Account Holder CNIC\n3.Account Balance\n4.Address")
+def update_employe():
+    global total_employe
+    print("What do you want to update :\n1.Employe Name\n2.Employe CNIC\n3.Employe Contact Number\n4.Employe Address")
     choice = eval(input("Enter your choice : "))
     if choice <= 4 and choice != 0:
-        temp_accnum = eval(input("Enter your account number : "))
+        temp_accnum = eval(input("Enter your ID : "))
         i = (temp_accnum - 1) * 4
-        if saving_account_registered != 0:
-            if ((temp_accnum * 4) <= len(saving_account_data)):
+        if total_employe != 0:
+            if ((temp_accnum * 4) <= len(employe_data)):
                 if choice == 1:
                     name = input("Enter Your Name : ")
-                    saving_account_data[i] = name
+                    employe_data[i] = name
                 elif choice == 2:
                     cnic = input("Enter Your CNIC number : ")
-                    saving_account_data[i + 1] = cnic
+                    employe_data[i + 1] = cnic
                 elif choice==3:
-                    balance = input("Enter Account Balance : ")
-                    saving_account_data[i + 2] = balance
+                    balance = input("Enter Contact Number : ")
+                    employe_data[i + 2] = balance
                 else:
                     address=input("Enter your Address : ")
-                    saving_account_data[i + 3] = address
+                    employe_data[i + 3] = address
             else:
                 print("No Record Found!")
-        elif saving_account_registered == 0:
-            print("No Account Created!")
+        elif total_employe == 0:
+            print("No Employe!")
     else:
         print("Invalid Choice!")
     print()
